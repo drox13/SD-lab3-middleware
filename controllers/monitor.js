09219a38cc108eb426c56617727924a726dee7da
AndresCrg <brayan.cardenas@uptc.edu.co>
2021-09-09 17:44:01 -0500

    Final

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,74 +1,101 @@
-const axios = require('axios');
+const axios = require("axios");
 const redis = require("redis");
 const { promisify } = require("util");
-const CircularList = require('../dataStructure/CircularList');
-const Node = require('../dataStructure/Node');
-const client = redis.createClient(4010, '127.0.0.1');
+const CircularList = require("../dataStructure/CircularList");
+const Node = require("../dataStructure/Node");
+const client = redis.createClient(4010, "127.0.0.1");
 const getAsync = promisify(client.get).bind(client);
+const fs = require('fs');
 
 const circularList = new CircularList();
 
 const PATH = process.cwd();
 
-let node2 = new Node('instancia2');
+let node2 = new Node("instancia2");
 circularList.addNode(node2);
 let instance = circularList.pointer.data;
 
-
-client.on("error", function(error) {
-    console.error(error);
+client.on("error", function (error) {
+  console.error(error);
 });
-  
 
 const postQuery = async (req, res) => {
-    const info = req.body.info;
-    const query = `SELECT * FROM tabla WHERE id == ${info}`;
-    const cacheQuery = await getAsync('query').toString();
+  const info = req.body.info;
+  const query = `SELECT * FROM tabla WHERE id == ${info}`;
+  const cacheQuery = await getAsync("query").toString();
 
-    //Esta parte es de pruebas
-    const data = 'Hola mundo';
-    client.set("query", query);
-    client.set("data", data);
-    res.send({
-        data
-    })
-    //hasta aca
+  //Esta parte es de pruebas
+  const data = "Hola mundo";
+  client.set("query", query);
+  client.set("data", data);
+  res.send({
+    data,
+  });
+  //hasta aca
 
-    // if (query == cacheQuery) {
-    //     Este data es de prueba, el data real debe llegar de redis
-    //     const data = 'Hola mundo';
-    //    const data = await getAsync('data');
-    //     res.send({
-    //         data
-    //     })
-    // }else{
-    //     haga la consulta al BD
-    //     client.set("query", query);
-    //     client.set("data", data);
-    //     res.send({
-    //         data
-    //     })
+  // if (query == cacheQuery) {
+  //     Este data es de prueba, el data real debe llegar de redis
+  //     const data = 'Hola mundo';
+  //    const data = await getAsync('data');
+  //     res.send({
+  //         data
+  //     })
+  // }else{
+  //     haga la consulta al BD
+  //     client.set("query", query);
+  //     client.set("data", data);
+        //  writeLog('\n Ok ' + instance + ' ' + new Date());
+				//  instance = circularList.nextPointer().data;
+  //     res.send({
+  //         data
+  //     })
+};
+
+let ram = 1;
+let cpu = 90;
+
+const postLimits = (req, res) => {
+  ram = req.body.ramData;
+  cpu = req.body.cpuData;
+  console.log(ram, cpu);
+  res.send({
+    msg: "ok",
+  });
+};
+
+const checkStats = () => {
+    if(ramAvalible < ram && cpuUsage > cpu){
+      let numberInstance = fs.readFileSync(PATH + '/Docker/counter.tmp', 'utf8');
+      circularList.addNode(new Node(`instancia${numberInstance}`));
+      circularList.showList();
+      shell.exec(PATH + `/Docker/newInstance.sh ${instance}` );
+		  numberInstance++
+		  fs.writeFileSync(PATH + '/docker/counter.tmp', numberInstance)
+    }
 }
 
-const getCpuUsage = axios.get('http://localhost:3000/resource/cpu')
+let cpuUsage = 0;
+let ramAvalible = 0;
+
+const getCpuUsage = axios
+  .get("http://localhost:3000/resource/cpu")
   .then(function (response) {
-    // handle success
-    console.log(response);
+    cpuUsage = response.data.cpuUsage;
+    console.log(cpuUsage);
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
-  })
+  });
 
-// function getRamAvalible() {
-//     //return axios.get(`http://119.18.0.${instance.charAt(instance.length - 1)}:3306/resource/ram`);
-//     return axios.get(`http://127.0.0.1:4003/resource/ram`);
-// }
-  
-// function getCpuUsage() {
-//   //return axios.get(`http://119.18.0.${instance.charAt(instance.length - 1)}:3306/resource/cpu`);
-//   return axios.get(`http://127.0.0.1:4003/resource/cpu`);
-// }
+const getRamAvalible = axios
+  .get("http://localhost:3000/resource/ram")
+  .then(function (response) {
+    ramAvalible = response.data.ramAvalible;
+    console.log(ramAvalible);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 function writeLog(data) {
   createFileLogs();
@@ -77,13 +104,16 @@ function writeLog(data) {
   });
 }
 
-const urlLogs = PATH + '/logers/Hystory.log';
+const urlLogs = PATH + "/dataLogs/Hystory.log";
 
 function createFileLogs() {
-	fs.open(urlLogs, 'r', function (err, f) {});
+  fs.open(urlLogs, "r", function (err, f) {});
 }
 
 module.exports = {
-    postQuery,
-    getCpuUsage
-}
+  postQuery,
+  getCpuUsage,
+  getRamAvalible,
+  postLimits,
+  checkStats
+};
--- models/server.js
+++ models/server.js
@@ -1,51 +1,45 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const PORT = 8000;
 
 class Server {
-    constructor(){
-        this.app = express();
-        this.server = require('http').createServer(this.app);
-        this.io = require('socket.io')(this.server);
-        this.port = PORT;
-        this.middleware();
-        this.routes();
-        this.sockets();
-    }
+  constructor() {
+    this.app = express();
+    this.server = require("http").createServer(this.app);
+    this.io = require("socket.io")(this.server);
+    this.port = PORT;
+    this.middleware();
+    this.routes();
+    this.sockets();
+  }
 
-    middleware(){
-        this.app.use(cors());
-        this.app.use(express.json());
-        this.app.use(express.static('public'));
-    }
+  middleware() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static("public"));
+  }
 
-    routes(){
-        this.app.use('/', require('../routes/monitor'));
-    }
+  routes() {
+    this.app.use("/", require("../routes/monitor"));
+  }
 
-    sockets(){
-        this.io.on('connection', client => {
-            console.log('Client connect!')
-            // client.on('event', data => { /* … */ });
-            // client.on('disconnect', () => { /* … */ });
-            
-            const payload = {
-                date: new Date().getTime,
-                ram: '',
-                cpu: '18%'
-            }
-            setInterval(() => {
-                this.io.emit('send-data-resource', payload);
-            }, 3000);
-          });
-    }
+  sockets() {
+    this.io.on("connection", (client) => {
+      console.log("Client connect!");
+      // client.on('event', data => { /* … */ });
+      // client.on('disconnect', () => { /* … */ });
 
-    listen(){
-        this.server.listen(this.port, ()=>{
-            console.log(`Server on ! PORT ${this.port}`);
-        });
-        this.io.on('connection', () => { /* … */ });
-    }
+      // setInterval(() => {
+      //   this.io.emit("send-data-resource", "home test");
+      // }, 3000);
+    });
+  }
+
+  listen() {
+    this.server.listen(this.port, () => {
+      console.log(`Server on ! PORT ${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
